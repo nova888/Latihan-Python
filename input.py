73784e6d53699736305122729f84e7f44d0c0a16
nova888 <nova.tampati@gmail.com>
2025-07-13 04:55:07 +0700

    tambah

---
+++ input.py
@@ -0,0 +1,11 @@
+# data = input("Masukkan data:")
+
+# print("data =", data, ".type =", type(data))
+
+# data_int = int(input("Masukan angka: "))
+# print("data =", data_int, ".type =", type(data_int10
+# ))
+
+biner = bool(int(input("Masukkan nilai boolean: ")))
+print("data =", biner,", type=", type(biner))
+
