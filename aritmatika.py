ce101e33315d2ef28e2c368a2ef32e0d46bf2cda
nova888 <nova.tampati@gmail.com>
2025-07-13 05:18:09 +0700

    tambah

---
+++ aritmatika.py
@@ -0,0 +1,37 @@
+#operasi aritmatika
+
+a = 10
+b = 3
+# operasi tambah +
+hasil = a+b
+print(a,'+',b,'=', hasil)
+
+#operasi pengurangan
+hasil = a-b
+print(a,'-',b,'=', hasil)
+
+# operasi perkalian
+hasil = a * b
+print(a,'*',b,'=', hasil)
+
+#operasi pembagian
+hasil = a / b
+print(a,'/',b,'=', hasil)
+
+#operasieksponen (pangkat)
+hasil = a ** b
+print(a,'**',b,'=', hasil)
+
+#operasi modulus %
+hasil = a % bprint(a,'%',b,'=', hasil)
+
+#operasi floor division
+hasil = a // b
+print(a,'//',b,'=', hasil)
+
+x = 3
+y = 2
+z = 4
+
+hasil = x + y * z
+print (x, '+', y, '*', z, '=', hasil)
