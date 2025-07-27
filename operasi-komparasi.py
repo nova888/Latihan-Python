3ae7bc908977760fb3be5f3128fd9b086a2f98da
nova888 <nova.tampati@gmail.com>
2025-07-27 19:22:30 +0700

    tambah

---
+++ operasi-komparasi.py
@@ -0,0 +1,11 @@
+# operasi komparasi
+
+# setiap hasil dari operasi komprasi adalah boolean
+
+#>,<,>=, <=, ==, !=, is, is not
+
+a = 4
+b = 2
+#lebih besar dari
+hasil = a > 3
+print(a, '<', 3, '=', hasil)
