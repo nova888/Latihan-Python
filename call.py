2d8f13682577534532546edd5b9460d6b1ea031b
nova888 <nova.tampati@gmail.com>
2025-07-27 19:06:32 +0700

    tambah

---
+++ call.py
@@ -0,0 +1,21 @@
+# Latihan konversi satuan temperature
+
+# program konversi celcius ke satuan lain
+
+print("\nPROGRAM KONVERSI TEMPERATUR\n")
+
+celcius = fload(input('Masukkan Suhu dalam celsius: '))
+print("suhu adalah", celcius, "Celcius")
+
+# reamur 
+reamur = (4/5) * celcius
+print("suhu dalam remaur adalah ", reamur, "Reamur")
+
+# fahrenheit
+fahrenhait = ((9/5) * celcius) +32
+print("suhu dalam fahrenheit adalah ", fahrenheit, "fahrenheit")
+
+
+# kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah", "kelvin")
