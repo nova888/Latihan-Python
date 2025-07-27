d534ae38120292ce807b056a9d076e1a744c2b74
nova888 <nova.tampati@gmail.com>
2025-07-27 19:16:36 +0700

    tambah

--- call.py
+++ call.py
@@ -4,7 +4,7 @@
 
 print("\nPROGRAM KONVERSI TEMPERATUR\n")
 
-celcius = fload(input('Masukkan Suhu dalam celsius: '))
+celcius = float(input('Masukkan Suhu dalam celsius: '))
 print("suhu adalah", celcius, "Celcius")
 
 # reamur 
@@ -13,9 +13,21 @@ print("suhu dalam remaur adalah ", reamur, "Reamur")
 
 # fahrenheit
 fahrenhait = ((9/5) * celcius) +32
-print("suhu dalam fahrenheit adalah ", fahrenheit, "fahrenheit")
+print("suhu dalam fahrenhait adalah ", fahrenhait, "fahrenheit")
 
 
 # kelvin
 kelvin = celcius + 273
 print("suhu dalam kelvin adalah", "kelvin")
+
+# fahrenheit ke kelvin
+fahrenheit = float(input("Masukkan Suhu Fahrenheit:"))
+celcius = (5/9) * (fahrenheit - 32)
+kelvin = (celcius + 273)
+print("suhu dalam kelvin :", kelvin)
+
+# kelvin ke farenheit
+kelvin = float(input("masukkan suhu dalam kelvin:"))
+celcius = kelvin - 273
+fahrenheit = ((9/5)*celcius) + 32
+print(" suhu dalam fahrenheit:" fahrenheit)
