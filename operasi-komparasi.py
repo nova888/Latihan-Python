5976934aebdc1b32f6bcbc175829ba98af025c5d
nova888 <nova.tampati@gmail.com>
2025-07-27 19:25:24 +0700

    tambah

--- operasi-komparasi.py
+++ operasi-komparasi.py
@@ -9,3 +9,11 @@ b = 2
 #lebih besar dari
 hasil = a > 3
 print(a, '<', 3, '=', hasil)
+hasil = b > 3
+print(b, '<', 3, '=', hasil)
+asil = b > 2
+print(b, '<',2, '=', hasil)
+
+#kurang dari
+asil = b < 3
+print(b, '<', 3, '=', hasil)
