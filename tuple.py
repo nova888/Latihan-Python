9e3e7f6364d625c289518d36bfb454828c98d10c
Nova Tampati <nova.tampati@gmail.com>
2026-05-03 12:27:07 +0700

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,9 @@
+empty_tuple = ()
+single_element_tuple = (5,)
+my_tuple = (1, 2, 3, "hello", 4.5)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+count = my_tuple[-1]
+count = my_tuple.count(2)
+index = my_tuple.index("hello")
