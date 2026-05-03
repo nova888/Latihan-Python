48338e6141f3fbcae8e6ca9ebd2654adfdc335f4
Nova Tampati <nova.tampati@gmail.com>
2026-05-03 12:45:01 +0700

    Add files via upload

---
+++ sat.py
@@ -0,0 +1,13 @@
+empty_set = set()
+my_set = {1, 2, 3, 4, 5}
+list_to_set = set([1, 2, 2, 3, 4])
+
+print(3 in my_set)
+my_set.add(6)
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+print(set1 | set2)
+print (set1 & set2)
+print(set1 - set2)
